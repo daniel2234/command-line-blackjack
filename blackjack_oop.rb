dead04781c44c573cc134d09c54113022bba08aa
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2016-07-18 20:41:09 -0400

    implemented the hit method

--- blackjack_oop.rb
+++ blackjack_oop.rb
@@ -7,11 +7,7 @@ class Card
   end
 
   def pretty_output
-   "The #{@face_value} of #{suit}"
-  end
-
-  def to_s
-    pretty_output
+    "The #{@face_value} of #{find_suit}"
   end
 
   def find_suit
@@ -23,18 +19,22 @@ class Card
               end
     ret_val
   end
+
+  def to_s
+    pretty_output
+  end
 end
 
 class Deck
   attr_accessor :cards
   def initialize
     @cards = []
-    ['H', 'S', 'D', 'C'].each do |suit|
-      ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'].each do |face_value|
+    ['H','D','S','C'].each do |suit|
+      ['2','3','4','5','6','7','8','9','10','J','Q','K','A'].each do |face_value|
         @cards << Card.new(suit, face_value)
       end
-    end 
-  scramble! 
+    end
+    scramble!
   end
 
   def scramble!
@@ -52,28 +52,28 @@ end
 
 module Hand
   def show_hand
-    puts "----#{name}'s Hand ----"
+    puts "---- #{name}'s Hand ----"
     cards.each do |card|
       puts "=> #{card}"
     end
-    puts "=> Total: #{total}" 
+    puts "=> Total: #{total}"
   end
 
   def total
-    face_values = cards.map { |card| card.face_value }
+    face_values = cards.map { |card| card.face_value  }
 
     total = 0
-    face_values.each do |val|
-      if val == "A"
+    face_values.each do |value|
+      if value == "A"
         total += 11
+      elsif value.to_i == 0
+        total += 10
       else
-        total += (val.to_i == 0 ? 10 : val.to_i)
+        total += value.to_i
       end
     end
-
-    face_values.select { |val| val == "A"}.count.times do
-      break if total <= 21
-      total -= 10
+    face_values.select{|e| e == "A"}.count.times do
+      total -= 10 if total > 21
     end
 
     total
@@ -88,7 +88,6 @@ module Hand
   end
 end
 
-
 class Player
   include Hand
 
@@ -97,6 +96,10 @@ class Player
   def initialize(n)
     @name = n
     @cards = []
+  end
+
+  def show_flop
+    show_hand
   end
 end
 
@@ -109,19 +112,102 @@ class Dealer
     @name = "Dealer"
     @cards = []
   end
+
+  def show_flop
+    puts "----Dealer's Hand----"
+    puts "=> first card is hidden"
+    puts "=> Second card is #{cards[1]}"
+  end
 end
 
 
-deck = Deck.new
+class Blackjack
+  attr_accessor :deck, :player, :dealer
 
-player = Player.new("Chris")
-player.add_card(deck.deal_one)
-player.add_card(deck.deal_one)
-player.show_hand
+  def initialize
+    @deck = Deck.new
+    @player = Player.new("Player1")
+    @dealer = Dealer.new
+  end
+
+  def set_player_name
+    puts "What is your name?"
+    player.name = gets.chomp
+  end
+
+  def deal_cards
+    player.add_card(deck.deal_one)
+    dealer.add_card(deck.deal_one)
+    player.add_card(deck.deal_one)
+    dealer.add_card(deck.deal_one)
+  end
+
+  def show_hands
+    player.show_flop
+    dealer.show_flop
+  end
+
+  def blackjack_or_bust?(player_or_dealer)
+    if player_or_dealer.total == 21
+      if player_or_dealer.is_a?(Dealer)
+        puts "Sorry, dealer hit blackjack. #{player.name} loses."
+      else
+        puts "Congratulations, you hit blackjack, #{player.name} win!"
+      end
+      exit
+    elsif player_or_dealer.is_busted?
+      if player_or_dealer.is_a?(Dealer)
+        puts "Congratulations, dealer busted. #{player.name} win!"
+      else
+        puts "Sorry, #{player.name} busted. #{player.name} loses."
+      end
+      exit
+    end
+  end
+
+  def player_turn
+    puts "#{player.name}'s turn."
+
+    blackjack_or_bust?(player)
+
+    while !player.is_busted?
+      puts "What would you like to do? 1)hit 2)stay"
+      response = gets.chomp
+
+      if !['1','2'].include?(response)
+        puts "Error: you must enter 1 or 2"
+        next
+      end
+
+      if response == '2'
+        puts "#{player.name} chose to stay."
+        break
+      end
+
+      #hit
+      new_card = deck.deal_one
+      puts "Dealing card to #{player.name}: #{new_card}"
+      player.add_card(new_card)
+      puts "#{player.name}'s total is now:#{player.total}"
+
+      blackjack_or_bust?(player)
+      end
+    puts "#{player.name} stays."
+  end
 
 
-dealer = Dealer.new
-dealer.add_card(deck.deal_one)
-dealer.add_card(deck.deal_one)
-dealer.show_hand
+  def start
+    set_player_name
+    deal_cards
+    show_hands
+    player_turn
+    # dealer_turn
+    # who_won?(player,dealer)
+  end
+end
+
+
+game = Blackjack.new
+game.start
+
 
